d470b752acfa18bb523a874204fd75cc6780a8f1
christophe gueroult <christophegueroult@gmail.com>
2020-04-22 16:32:24 +0100

    viewencapsulation

---
+++ src/app/shared/components/table-light/table-light.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-table-light',
+  templateUrl: './table-light.component.html',
+  styleUrls: ['./table-light.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TableLightComponent implements OnInit {
+  @Input() headers: string[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TotalPipe } from './pipes/total.pipe';
+import { NgModule } from '@angular/core';
+import { TableLightComponent } from './components/table-light/table-light.component';
 import { StateDirective } from './directives/state.directive';
+import { TotalPipe } from './pipes/total.pipe';
+import { TableDarkComponent } from './components/table-dark/table-dark.component';
 
 
 
 @NgModule({
-  declarations: [TotalPipe, StateDirective],
-  exports: [TotalPipe, StateDirective],
+  declarations: [TotalPipe, StateDirective, TableLightComponent, TableDarkComponent],
+  exports: [TotalPipe, StateDirective, TableLightComponent, TableDarkComponent],
   imports: [
     CommonModule
   ]
